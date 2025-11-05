d9543a5728aae423624c22313906b2002b81780c
Alina Maria <linamaria.ciber@gmail.com>
2025-11-05 16:05:01 -0500

    refactor: remove obsolete domain entities. Ref #10

--- backend/src/Domain/Entities/DestinyType.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Entities
-{
-    public class DestinyType
-    {
-        public int Id { get; set; }
-        public string Type { get; set; } = string.Empty; //deshecho, almacén o sección
-        public List<TechnicalDowntime> TechnicalDowntimes { get; set; } = new List<TechnicalDowntime>()!;
-    }
-}
