711e8b96428caf07bbe5839fb310547b1f1a8f9b
fabio2k3 <fvab2k3@gmail.com>
2025-11-26 20:16:30 -0500

    feat: Add User DTO. Ref #8

---
+++ backend/src/Application/DTOs/User/UpdateUserDto.cs
@@ -0,0 +1,9 @@
+namespace Application.DTOs.User
+{
+    public class UpdateUserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
+
