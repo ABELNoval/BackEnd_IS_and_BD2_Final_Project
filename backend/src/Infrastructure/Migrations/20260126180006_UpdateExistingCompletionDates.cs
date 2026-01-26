1e7f3d453437385b81429a897ba43577972ecf6a
Abel de la Noval <ryuto1234yolo@gmail.com>
2026-01-26 13:10:41 -0500

    feat: Add migration to update CompletionDate in EquipmentDecommissions; set CompletionDate to DecommissionDate where NULL

---
+++ backend/src/Infrastructure/Migrations/20260126180006_UpdateExistingCompletionDates.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateExistingCompletionDates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Update existing records: set CompletionDate = DecommissionDate where NULL
+            migrationBuilder.Sql(@"
+                UPDATE EquipmentDecommissions 
+                SET CompletionDate = DecommissionDate 
+                WHERE CompletionDate IS NULL
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ backend/src/Infrastructure/Migrations/20260126180654_FixCompletionDatesData.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCompletionDatesData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Update existing records: set CompletionDate = DecommissionDate where NULL
+            migrationBuilder.Sql(@"
+                UPDATE EquipmentDecommissions 
+                SET CompletionDate = DecommissionDate 
+                WHERE CompletionDate IS NULL
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Revert: set CompletionDate back to NULL
+            migrationBuilder.Sql(@"
+                UPDATE EquipmentDecommissions 
+                SET CompletionDate = NULL
+            ");
+        }
+    }
+}
